2163de4eb1a5bd297fd47e88b18b0a419ab6f7c7
seraphinush <30970630+seraphinush@users.noreply.github.com>
2017-10-06 12:07:53 +0900

    Update index.js
    - Personalized code aesthetics
    - Added Command support
    - Added on/off toggle

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-module.exports = function SkipCutscenes(dispatch) {
-  dispatch.hook('sPlayMovie', (event) => {
-    dispatch.toServer('cEndMovie', Object.assign({ unk: true }, event));
-    return false;
-  });
-};
+// OPCODE REQUIRED :
+// - C_END_MOVIE
+// - S_PLAY_MOVIE
+
+module.exports = function SkipCutscene(dispatch) {
+
+	let enable = true
+
+	// command
+	try {
+		const Command = require('command')
+		const command = Command(dispatch)
+		command.add('skip', () => {
+			enable = !enable
+			send(` : Skip Cutscene ${enable ? '<font color="#56B4E9">enabled</font>' : '<font color="#E69F00">disabled</font>'}.`)
+		})
+		function send(msg) {
+			command.message(`<font color="#FFFFFF">` + msg + `</font>`)
+		}
+	} catch (e) {
+		console.log(`[ERROR] -- Skip Cutscene module --`)
+	}
+
+	// code
+	dispatch.hook('S_PLAY_MOVIE', (event) => {
+		if (enable) {
+			dispatch.toServer('C_END_MOVIE', Object.assign({ unk: true }, event))
+			return false
+		}
+	})
+
+}
