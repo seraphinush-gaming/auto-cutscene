c1fb9231575d7073c2b88fd338575290cf9ead77
seraph <30970630+seraphinush-gaming@users.noreply.github.com>
2018-01-01 17:45:54 +0900

    Update index.js -- 1.27 Updated name && font.clr

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 // - C_END_MOVIE
 // - S_PLAY_MOVIE
 
-// Version 1.26 r:00
+// Version 1.27 r:00
 
-module.exports = function SkipCutscene(d) {
+module.exports = function AutoCutscene(d) {
 
 	let enable = true
 
@@ -22,15 +22,15 @@ module.exports = function SkipCutscene(d) {
 		// NA
 		command.add('skip', () => {
 			enable = !enable
-			send(`${enable ? 'enabled'.clr('5AFF39') : 'disabled'.clr('E69F00')}` + `.`.clr('FFFFFF'))
+			send(`${enable ? 'enabled'.clr('56B4E9') : 'disabled'.clr('E69F00')}` + `.`.clr('FFFFFF'))
 		})
 		// KR
 		command.add('스킵', () => {
 			enable = !enable
-			send(`${enable ? '실행되었습니다'.clr('5AFF39') : '중지되었습니다'.clr('E69F00')}` + `.`.clr('FFFFFF'))
+			send(`${enable ? '실행되었습니다'.clr('56B4E9') : '중지되었습니다'.clr('E69F00')}` + `.`.clr('FFFFFF'))
 		})
-		function send(msg) { command.message(`[skip-cutscene] : ` + [...arguments].join('\n\t - ')) }
-	} catch (e) { console.log(`[ERROR] -- Skip Cutscene module --`) }
+		function send(msg) { command.message(`[auto-cutscene] : ` + [...arguments].join('\n\t - ')) }
+	} catch (e) { console.log(`[ERROR] -- auto-cutscene module --`) }
 
 }
 
