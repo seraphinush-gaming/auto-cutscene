76821cb59001422c523a78f80fc9ed02f07cf7c7
seraph <seraphinush@gmail.com>
2020-05-03 07:32:30 +0900

    1.31.01

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ class auto_cutscene {
 
   constructor(mod) {
 
-		this.m = mod;
-		this.c = mod.command;
-		this.s = mod.settings;
+    this.m = mod;
+    this.c = mod.command;
+    this.s = mod.settings;
 
-		// command
-		this.c.add('skip', {
-			'$default': () => {
-				this.s.enable = !this.s.enable;
-				this.send(`${this.s.enable ? 'En' : 'Dis'}abled`)
-			}
-		});
+    // command
+    this.c.add('skip', {
+      '$default': () => {
+        this.s.enable = !this.s.enable;
+        this.send(`${this.s.enable ? 'En' : 'Dis'}abled`)
+      }
+    });
 
-		// code
+    // code
     this.m.hook('S_PLAY_MOVIE', 1, (e) => {
       if (this.s.enable) {
         this.m.send('C_END_MOVIE', 1, Object.assign({ unk: 1 }, e));
@@ -26,9 +26,9 @@ class auto_cutscene {
 
   }
 
-	destructor() {}
-	
-	send(msg) { this.c.message(': ' + msg); }
+  destructor() {}
+  
+  send(msg) { this.c.message(': ' + msg); }
 
 }
 
